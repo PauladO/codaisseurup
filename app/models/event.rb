47b30f9e7f743631bc77daa505419cfc659c5ae4
PauladO <pauladenouden@gmail.com>
2017-03-14 11:50:48 +0100

    Set column defauts in events, price, includes_food, includes_drinks, active

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,7 @@ class Event < ApplicationRecord
 
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 500}
+  validates: :starts_at, presence: true
+  validates: :ends_at, :greater_than :starts_at + 1
+
 end
---
+++ db/migrate/20170314094723_add_defaults_to_events.rb
@@ -0,0 +1,8 @@
+class AddDefaultsToEvents < ActiveRecord::Migration[5.0]
+  def change
+    change_column :events, :price, :decimal, default: 0
+    change_column :events, :includes_food, :boolean, default: false
+    change_column :events, :includes_drinks, :boolean, default: false
+    change_column :events, :active, :boolean, default: true
+  end
+end
