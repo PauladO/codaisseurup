c63e4db366d04a3651c85b6efd3a1be6c0724218
PauladO <pauladenouden@gmail.com>
2017-03-14 16:07:27 +0100

    Edit event index and show

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,13 @@ class Event < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: {maximum: 500}
   validates :starts_at, presence: true
-  #validates :ends_at, after: :starts_at + 1.hour
+  validate :starts_at_must_be_before_ends_at_time
+
+  private
+
+  def starts_at_must_be_before_ends_at_time
+    errors.add(:starts_at, "must be before end time") unless
+        starts_at < ends_at
+  end
 
 end
